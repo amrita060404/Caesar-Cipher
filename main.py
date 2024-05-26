d29eae3d962c2474c1642ce70f4f8d9f3dd4e261
amrita060404 <147573723+amrita060404@users.noreply.github.com>
2024-05-26 17:08:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}\n")
+
+from art import logo
+print(logo)
+
+ 
+do_continue = True
+while do_continue == True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  if direction != ("encode" or "decode"):
+    print("Uh-oh! Seems like your input is not a valid. Please enter a valid input.\n")
+    exit
+  else:
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  restart_program = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart_program == "no":
+    do_continue = False
+    print("Goodbye")
